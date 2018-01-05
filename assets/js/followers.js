f16a64937fa7a0c6a775188e818527f2b1bd5f30
carocelis <carolina.celis.sepulveda@gmail.com>
2018-01-05 13:29:00 -0300

    solucion actualización de estado

--- assets/js/followers.js
+++ assets/js/followers.js
@@ -2,5 +2,5 @@ $('#send').click(function(){
 	var message = $('#status').val();
 	$('#status').val("");
 	var msgcont = $('#contenedor');
-	msgcont.append('<div>' + '<p>' + status + '</p>' + '</div>');
+	msgcont.append('<div>' + '<p>' + message + '</p>' + '</div>');
 });

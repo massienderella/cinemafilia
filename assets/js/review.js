e9e77f4d736d15bb078d8000fd720d4c9021f904
carocelis <carolina.celis.sepulveda@gmail.com>
2018-01-05 02:38:48 -0300

    post foto y muestra reseñas review

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   var imgItems = $('.movie-review blockquote').length; //Número de imágenes
 
 
@@ -8,18 +8,79 @@ $(document).ready(function(){
 
 
 //Ejecución de las funciones
-  $('.paginations li').click(paginations);
+  $('.my-review img').click(paginations);
   $('.chevronLeft').click(prevImg);
   $('.chevronRight').click(SigImg);
 
   function paginations(){
     var imgPosition = $(this).index() + 1; //Posición de cada ícono
 
-    $('.thumbnail img').hide(); 
-    $('.thumbnail img:nth-child('+ imgPosition +')').fadeIn();  //Muestra imagen según posición de ícono
-
-    $('.paginations li').css({'color': '#797C74'});
-    $(this).css({'color': 'white'});
+    $('.movie-review blockquote').hide(); 
+    $('.movie-review blockquote:nth-child('+ imgPosition +')').fadeIn();  //Muestra imagen según posición de ícono
   }
 
+});*/
+
+
+var idPin = 0;
+
+$(document).ready(function(){
+  $('.movie-review blockquote').hide(); //ocultar todas las reseñas
 });
+
+function savePin(){
+  //tomar valores de los input
+  var titulo = $("#tituloInput").val();
+  var descripcion = $("#descripcionInput").val();
+  var image = $(".image").val();
+
+  $('.movie-review blockquote').hide();
+  
+  //mostrar post
+  $('.post').show();
+  $('.post').prepend("<blockquote id='post_" + idPin + "' class='blockquote-reverse'>" +
+    "<img src='" + image + "' alt='...'>" +
+    "<footer>" + titulo + "</footer>" +
+    "<p>" + descripcion + "</p>" +
+    "</blockquote");
+};
+  //mostrar "mis reseñas" dando click a la imagen 
+$('.leon').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-leon').fadeIn();
+  });
+
+$('.inter').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-inter').fadeIn();
+  });
+
+$('.secreto').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-secreto').fadeIn();
+  });
+
+$('.million').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-million').fadeIn();
+  });
+
+$('.taxi').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-taxi').fadeIn();
+  });
+
+$('.whiplash').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-whiplash').fadeIn();
+  });
+
+$('.cidade').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-cidade').fadeIn();
+  });
+
+$('.broken').click(function(){
+    $('.movie-review blockquote').hide();
+    $('.bl-broken').fadeIn();
+  });

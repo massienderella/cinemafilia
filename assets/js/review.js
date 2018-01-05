54f24587cbf1beb2c85a45522944e4e0fe87a8a5
carocelis <carolina.celis.sepulveda@gmail.com>
2018-01-05 13:16:52 -0300

    semi solucion foto post review

--- assets/js/review.js
+++ assets/js/review.js
@@ -22,28 +22,101 @@
 });*/
 
 
+
+
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  
+   //this is not completely neccesary, just a nice function I found to make the file size format friendlier
+  //http://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable
+  function humanFileSize(bytes, si) {
+      var thresh = si ? 1000 : 1024;
+      if(bytes < thresh) return bytes + ' B';
+      var units = si ? ['kB','MB','GB','TB','PB','EB','ZB','YB'] : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+      var u = -1;
+      do {
+          bytes /= thresh;
+          ++u;
+      } while(bytes >= thresh);
+      return bytes.toFixed(1)+' '+units[u];
+  }
+
+
+  //this function is called when the input loads an image
+  function renderImage(file){
+    var reader = new FileReader();
+    reader.onload = function(event){
+      the_url = event.target.result
+      //of course using a template library like handlebars.js is a better solution than just inserting a string
+      
+     // function savePin(){
+  //tomar valores de los input
+  var titulo = $("#tituloInput").val();
+  var descripcion = $("#descripcionInput").val();
+  //var image = $(".image").val();
+
+  $('.movie-review blockquote').hide();
+
+  //mostrar post
+  $('.post').show();
+  $('.post').prepend("<blockquote id='post_" + idPin + "' class='blockquote-reverse'>" +
+    "<img src='" + the_url + "' alt='...'>" +
+    "<footer>" + titulo + "</footer>" +
+    "<p>" + descripcion + "</p>" +
+    "</blockquote");
+//};   
+
+}
+    
+    //when the file is read it triggers the onload event above.
+    reader.readAsDataURL(file);
+  }
+
+  
+
+  //watch for change on the 
+  $( "#exampleInputFile" ).change(function() {
+    console.log("photo file has been chosen")
+    //grab the first image in the fileList
+    //in this example we are only loading one file.
+    console.log(this.files[0].size)
+    renderImage(this.files[0])
+
+  });
+  
+} else {
+
+  alert('The File APIs are not fully supported in this browser.');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var idPin = 0;
 
 $(document).ready(function(){
   $('.movie-review blockquote').hide(); //ocultar todas las reseñas
 });
 
-function savePin(){
-  //tomar valores de los input
-  var titulo = $("#tituloInput").val();
-  var descripcion = $("#descripcionInput").val();
-  var image = $(".image").val();
 
-  $('.movie-review blockquote').hide();
-  
-  //mostrar post
-  $('.post').show();
-  $('.post').prepend("<blockquote id='post_" + idPin + "' class='blockquote-reverse'>" +
-    "<img src='" + image + "' alt='...'>" +
-    "<footer>" + titulo + "</footer>" +
-    "<p>" + descripcion + "</p>" +
-    "</blockquote");
-};
+
+
+
+
   //mostrar "mis reseñas" dando click a la imagen 
 $('.leon').click(function(){
     $('.movie-review blockquote').hide();
